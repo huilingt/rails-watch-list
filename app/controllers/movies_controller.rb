c221d791fd94b9f753d4f0561869cb67041eca8f
Huiling Tan <huilingt@hotmail.com>
2022-05-27 15:31:05 +0800

    watchlist movies controller index

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,48 @@
+class MoviesController < ApplicationController
+  # before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @movies = Movie.all
+  end
+
+  # def show
+  # end
+
+  # def new
+  #   @movie = Movie.new
+  # end
+
+  # def create
+  #   @movie = Movie.new(movie_params)
+  #   if @movie.save
+  #     redirect_to movie_path(@movie)
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  #   @movie.update(movie_params)
+
+  #   redirect_to movie_path(@movie)
+  # end
+
+  # def destroy
+  #   @movie.destroy
+
+  #   redirect_to movies_path
+  # end
+
+  # private
+
+  # def movie_params
+  #   params.require(:movie).permit(:name, :address, :phone_number, :category)
+  # end
+
+  # def set_movie
+  #   @movie = Movie.find(params[:id])
+  # end
+end
