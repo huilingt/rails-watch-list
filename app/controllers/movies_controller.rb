8d7572979539253456c2108865414de34366f8d7
Huiling Tan <huilingt@hotmail.com>
2022-05-27 16:14:57 +0800

    movies controller complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,48 +1,48 @@
 class MoviesController < ApplicationController
-  # before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
   def index
     @movies = Movie.all
   end
 
-  # def show
-  # end
+  def show
+  end
 
-  # def new
-  #   @movie = Movie.new
-  # end
+  def new
+    @movie = Movie.new
+  end
 
-  # def create
-  #   @movie = Movie.new(movie_params)
-  #   if @movie.save
-  #     redirect_to movie_path(@movie)
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
+  end
 
-  # def edit
-  # end
+  def edit
+  end
 
-  # def update
-  #   @movie.update(movie_params)
+  def update
+    @movie.update(movie_params)
 
-  #   redirect_to movie_path(@movie)
-  # end
+    redirect_to movie_path(@movie)
+  end
 
-  # def destroy
-  #   @movie.destroy
+  def destroy
+    @movie.destroy
 
-  #   redirect_to movies_path
-  # end
+    redirect_to movies_path
+  end
 
-  # private
+  private
 
-  # def movie_params
-  #   params.require(:movie).permit(:name, :address, :phone_number, :category)
-  # end
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :rating, :poster_url)
+  end
 
-  # def set_movie
-  #   @movie = Movie.find(params[:id])
-  # end
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
 end
