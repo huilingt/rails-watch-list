735b1b834a46443a3ee7d1b16c88d13bce3d842f
Huiling Tan <huilingt@hotmail.com>
2022-05-27 14:31:20 +0800

    change rating to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2022_05_27_055825) do
     t.string "title"
     t.string "overview"
     t.string "poster_url"
-    t.integer "rating"
+    t.decimal "rating", precision: 2, scale: 1
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
